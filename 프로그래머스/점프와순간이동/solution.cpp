383bec4f2877cc8b92a5221246abd32303dbb48c
SeungJoonChoi <tmdwns0845@naver.com>
2024-08-18 14:46:43 +0900

    algorithm

---
+++ 프로그래머스/점프와순간이동/solution.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int ans = 0;
+    while (n) {
+        if (n % 2) {
+            ++ans;
+            --n;
+        } else
+            n /= 2;
+    }
+    return ans;
+}
