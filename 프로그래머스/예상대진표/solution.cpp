7202dca238d73b29580e8c0a31ca7bc7beff9d62
SeungJoonChoi <tmdwns0845@naver.com>
2024-08-18 20:22:55 +0900

    algorithm

---
+++ 프로그래머스/예상대진표/solution.cpp
@@ -0,0 +1,13 @@
+using namespace std;
+
+int solution(int n, int a, int b) {
+    int ans = 0;
+    --a;
+    --b;
+    while (a != b) {
+        ++ans;
+        a /= 2;
+        b /= 2;
+    }
+    return ans;
+}
