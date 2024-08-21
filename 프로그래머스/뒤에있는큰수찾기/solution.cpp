edf6d3257ce58053b641e1e81b10193cbc96424b
SeungJoonChoi <tmdwns0845@naver.com>
2024-08-21 21:30:47 +0900

    algorithm

---
+++ 프로그래머스/뒤에있는큰수찾기/solution.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers) {
+    vector<int> ans(numbers.size(), -1);
+    
+    stack<int> stk;
+    for (int i = 0; i < numbers.size(); ++i) {
+        while (!stk.empty() && numbers[stk.top()] < numbers[i]) {
+            ans[stk.top()] = numbers[i];
+            stk.pop();
+        }
+        stk.push(i);
+    }
+    
+    return ans;
+}
