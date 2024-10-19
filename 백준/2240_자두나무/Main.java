2169965ca350d0040b7aad9da5c47798543bff5b
Seungjoon Choi <tmdwns0845@naver.com>
2024-10-19 15:52:01 +0900

    algorithm

---
+++ 백준/2240_자두나무/Main.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    private static int T;
+    private static int W;
+    private static int[] arr;
+    private static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        T = Integer.parseInt(st.nextToken());
+        W = Integer.parseInt(st.nextToken());
+        arr = new int[T + 1];
+        dp = new int[W + 1][T + 1];
+
+        for (int i = 1; i <= T; ++i) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = 1; i <= T; ++i) {
+            for (int j = 0; j <= W && j <= i; ++j) {
+                if (j == 0) {
+                    dp[j][i] = dp[j][i - 1];
+                } else {
+                    dp[j][i] = Math.max(dp[j - 1][i - 1], dp[j][i - 1]);
+                }
+
+                int pos = j % 2 == 0 ? 1 : 2;
+                if (pos == arr[i]) {
+                    dp[j][i]++;
+                }
+            }
+        }
+
+        int answer = 0;
+        for (int i = 0; i <= W; ++i) {
+            answer = Math.max(answer, dp[i][T]);
+        }
+        System.out.print(answer);
+    }
+}
