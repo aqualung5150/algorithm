ac4f2bb6db4ba154126eebf7f7b16ece413ab91d
Seungjoon Choi <tmdwns0845@naver.com>
2025-04-11 23:49:01 +0900

    algorithm

---
+++ gold_random_defense/12015_가장긴증가하는부분수열2/Main.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    private static int N;
+    private static int[] arr, dp;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; ++i) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dp = new int[N];
+        dp[0] = arr[0];
+        int lastIdx = 0;
+        for (int i = 1; i < N; ++i) {
+            int cur = arr[i];
+            if (cur > dp[lastIdx]) {
+                ++lastIdx;
+                dp[lastIdx] = cur;
+            } else {
+                int insertIdx = lowerBound(cur, 0, lastIdx);
+                dp[insertIdx] = cur;
+            }
+        }
+
+        System.out.print(lastIdx + 1);
+    }
+
+    private static int lowerBound(int target, int left, int right) {
+        while (left < right) {
+            int mid = (left + right) / 2;
+
+            if (target > dp[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return right;
+    }
+}
